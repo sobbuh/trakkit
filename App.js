d144f5b9c991d43fe3cce9bebca5b3d326e1b6da
sobbuh <sobbuh7@gmail.com>
2017-09-23 21:25:04 -0700

    added tab nav

--- App.js
+++ App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { View, Platform, StatusBar } from 'react-native';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import Feed from './components/Feed'
+import Profile from './components/Profile'
+import Explore from './components/Explore'
+import Cohorts from './components/Cohorts'
+import Record from './components/Record'
 
 function TrakkitStatusBar ({backgroundColor, ...props}) {
   return (
@@ -26,6 +31,61 @@ const MainNavigator = StackNavigator({
   }
 })
 
+const Tabs = TabNavigator({
+  Feed: {
+    screen: Feed,
+    navigationOptions: {
+      tabBarLabel: 'Feed',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ion-ios-list' size={30} color={tintColor} />
+    }
+  },
+  History: {
+    screen: Cohorts,
+    navigationOptions: {
+      tabBarLabel: 'Cohorts',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ion-ios-people' size={30} color={tintColor} />
+    },
+  },
+  Record: {
+    screen: Record,
+    navigationOptions: {
+      tabBarLabel: 'Record',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='ion-android-radio-button-on' size={30} color={tintColor} />
+    },
+  },
+  Explore: {
+    screen: Explore,
+    navigationOptions: {
+      tabBarLabel: 'Explore',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ion-compass' size={30} color={tintColor} />
+    }
+  },
+  Profile: {
+    screen: Profile,
+    navigationOptions: {
+      tabBarLabel: 'Profile',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-person' size={30} color={tintColor} />
+    }
+  }
+}, {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
 
 export default class App extends React.Component {
   render() {
